5152c6555c6b2698f761345d1fedf6e6c1f9db28
wkyle <wrkyle@mail.com>
2017-10-28 20:30:36 -0600

    facebook and twitter modules added

--- dev/wes/code/js/politician_modules/mp_modules.js
+++ dev/wes/code/js/politician_modules/mp_modules.js
@@ -1,14 +1,16 @@
 
-//*******************************************************************************************
-//
-//		VOTING RECORD MODULE
-//
-//*******************************************************************************************
+
+
+
+
 
 $(document).ready(function() {
 	loadMPDatabase("../../../data/government/mp_list.xml");
-	var maincontainer = d3.select("#main-container");
-	var votingmodule = maincontainer.append("div").classed("module-container", true).attr("id", "mp-voting-record");
+	loadVotingRecord();
+	initTwitter();
+	initFacebookFeed();
+
+
 });
 
 
@@ -23,4 +25,205 @@ function loadMPDatabase(filelocation) {
   	};
   	xhttp.open("GET", filelocation, true);
   	xhttp.send();
+}
+
+
+
+
+
+
+//*******************************************************************************************
+//
+//		VOTING RECORD MODULE
+//
+//*******************************************************************************************
+
+
+
+
+class PieChart {
+
+	constructor(data, containerselector) {
+		this.data = data;
+		this.elementId = containerselector;
+		// this.canvas = d3.select(containerselector);
+	}
+
+	initChart() {
+		var containerEl = document.getElementById( this.elementId ),
+	        width       = containerEl.clientWidth,
+	        height      = width,
+	        radius      = Math.min( width, height ) / 2,
+	        container   = d3.select( containerEl ),
+	        svg         = container.append( 'svg' )
+	        					  .attr("id", "pieChartSVG")
+	                              .attr( 'width', width )
+	                              .attr( 'height', height );
+	    var pie = svg.append( 'g' )
+	                .attr(
+	                  'transform',
+	                  'translate(' + width / 2 + ',' + height / 2 + ')'
+	                );
+
+	    var detailedInfo = svg.append( 'g' )
+	                          .attr( 'class', 'pieChart--detailedInformation' );
+
+	    var twoPi   = 2 * Math.PI;
+	    var pieData = d3.pie()
+	                    .value( function( d ) { return d.value; } );
+
+
+	    var arc = d3.arc()
+	                    .outerRadius( radius - 10)
+	                    .innerRadius( radius - 60 );
+
+	    var pieChartPieces = pie.datum( this.data )
+	                            .selectAll( 'path' )
+	                            .data( pieData )
+	                            .enter()
+	                            .append( 'path' )
+	                            .attr( 'class', function( d ) {
+	                              return 'pieChart__' + d.data.color;
+	                            } )
+	                            .attr( 'd', arc )
+	                            .each( function() {
+	                              this._current = { startAngle: 0, endAngle: 0 };
+	                            } )
+	                            .transition()
+	                            .duration( 600 )
+	                            .attrTween( 'd', function( d ) {
+	                              var interpolate = d3.interpolate( this._current, d );
+	                              this._current = interpolate( 0 );
+	                              console.log(this._current)
+
+	                              return function( t ) {
+	                                return arc( interpolate( t ) );
+	                              };
+	                            } )
+	                            // .each( 'end', function handleAnimationEnd( d ) {
+	                            //   // drawDetailedInformation( d.data, this );
+	                            // } );
+	}
+
+	eraseChart() {
+
+	}
+
+	resetChart() {
+
+	}
+}
+
+
+
+
+
+
+
+function loadVotingRecord() {
+	var piedata = [
+      {
+        color       : 'green',
+        description : 'Member voted in favour of motion or bill.',
+        title       : 'Yea',
+        value       : 0.62
+      },
+      {
+        color       : 'blue',
+        description : 'Member voted in opposition to motion or bill.',
+        title       : 'Nay',
+        value       : 0.28
+      },
+      {
+        color       : 'gray',
+        description : 'Member was absent, abstained, or was paired for vote.',
+        title       : 'None',
+        value       : 0.1
+      }
+    ]
+    var votingmodule = d3.select("#main-container").append("div").classed("module-container", true).attr("id", "voting-record");
+	votingmodule.append("h2").text("Voting Record Module");
+	var pie = new PieChart(piedata, "voting-record");
+	pie.initChart();
+}
+
+
+
+
+
+
+
+
+
+//*******************************************************************************************
+//
+//		TWITTER MODULE
+//
+//*******************************************************************************************
+
+
+function initTwitter() {
+	window.twttr = (function(d, s, id) {
+			var js, fjs = d.getElementsByTagName(s)[0],
+				t = window.twttr || {};
+			if (d.getElementById(id)) return t;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "https://platform.twitter.com/widgets.js";
+			fjs.parentNode.insertBefore(js, fjs);
+
+			t._e = [];
+			t.ready = function(f) {
+				t._e.push(f);
+			};
+
+			return t;
+		}(document, "script", "twitter-wjs"));
+	twttr.ready(function(twttr) {
+		loadTwitterFeed("https://twitter.com/JustinTrudeau");
+	});
+}
+
+
+
+function loadTwitterFeed(address) {
+	screenname = address.split("/")[address.split("/").length - 1];
+	d3.select("#main-container")
+		.append("a")
+	    .classed("twitter-timeline", true)
+	    .classed("module-container", true)
+	    .attr("id","twitter-timeline")
+	    .attr("href", address);
+	twttr.widgets.createTimeline(
+	  {
+	    sourceType: "profile",
+	    screenName: screenname
+	  },
+	  document.getElementById("twitter-timeline"),
+	  {
+	  	height: "800"
+	  }
+	);
+}
+
+
+
+
+
+
+
+
+function initFacebookFeed() {
+	pagelink = encodeURIComponent("https://www.facebook.com/JustinPJTrudeau");
+	d3.select("#main-container")
+		.append("iframe")
+		.classed("module-container", true)
+		.attr("id", "facebook-timeline")
+		.attr("src", "https://www.facebook.com/plugins/page.php?href=" + pagelink + "&tabs=timeline&width=340&height=800&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=false&appId")
+		.attr("width", "340")
+		.attr("height", "800")
+		.attr("style", "border:none;overflow:hidden")
+		.attr("scrolling", "no")
+		.attr("frameborder", "0")
+		.attr("allowTransparency", "true");
 }
--- dev/wes/code/python/DBUpdate.py
+++ dev/wes/code/python/DBUpdate.py
@@ -32,20 +32,39 @@ def getStatusCode(url):
 
 
 def retreiveXML(url):
-    xml_string = requests.get(url).content
-    xml_root = ET.fromstring(xml_string)
-    return xml_root
+    code = getStatusCode(url)
+    if str(code)[0] == "2":
+        xml_string = requests.get(url).content
+        xml_root = ET.fromstring(xml_string)
+        return xml_root
+    else:
+        return None
 
 
-code = getStatusCode(mp_xml_url)
-if str(code)[0] == "2":
-    xml = retreiveXML(mp_xml_url)
-    tree = ET.ElementTree(xml)
-    for mp in xml:
-        name = mp.find("PersonOfficialFirstName").text
-        surname = mp.find("PersonOfficialLastName").text
-        name = "-".join([name,surname])
-        vote_record_url = "http://www.ourcommons.ca/Parliamentarians/en/members/" + name + "/ExportVotes?output=XML"
-        voting_record = ET.SubElement(mp,"VotingRecordXML")
-        voting_record.text = vote_record_url
-    tree.write(output_file, encoding="UTF-8")
+
+
+root = retreiveXML(mp_xml_url)
+tree = ET.ElementTree(root)
+for mp in root:
+    name = mp.find("PersonOfficialFirstName").text
+    surname = mp.find("PersonOfficialLastName").text
+    name = "-".join([name,surname])
+    vote_record_url = "http://www.ourcommons.ca/Parliamentarians/en/members/" + name + "/ExportVotes?output=XML"
+    member_voting_record = ET.SubElement(mp, "MemberVotingRecord")
+    voting_record = ET.SubElement(member_voting_record,"VotingRecordXML")
+    voting_events = ET.SubElement(member_voting_record, "VotingEvents")
+    voting_record.text = vote_record_url
+    vote_record_root = retreiveXML(vote_record_url)
+    for vote in vote_record_root:
+        try:
+            vote.remove(vote.find("PersonOfficialFirstName"))
+            vote.remove(vote.find("PersonaOfficialLastName"))
+            vote.remove(vote.find("PersonShortHonorific"))
+            vote.remove(vote.find("ConstituencyName"))
+            vote.remove(vote.find("ConstituencyProvinceTerritoryName"))
+        except:
+            pass
+        voting_events.append(vote)
+        
+            
+tree.write(output_file, encoding="UTF-8")
